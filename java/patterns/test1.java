cd764e95699d96be02c80903f592817b2947f517
stubanx <gantavyaoo@gmail.com>
2024-09-20 08:55:14 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   OOP concept/java/staticBlock.class
    	new file:   OOP concept/java/staticBlock.java
    	new file:   java/patterns/box.class
    	new file:   java/patterns/box.java
    	modified:   java/patterns/test1.java
    
    Changes not staged for commit:
    	modified:   python/image analysis (modified content)
    	modified:   web/my-app (modified content)

--- java/patterns/test1.java
+++ java/patterns/test1.java
@@ -12,8 +12,13 @@ public class test1 {
         s2.age = 23;
         s1.intro();
         s2.intro();
+        
         // CLASS:- A class is a user defined blueprint or prototype from which object are created.
         // OBJECT:- 
-
+        // THIS KEYWORD:- Refers to the current object in a method or constructor
+        // A constructor is a special method that is used to initialise spe
+        // the constructor is called when an object of a class is created.
+    
+    
     }
 }
