e3dffede1b2092799e1bdab54d6c5c8af24104b9
stubanx <gantavyaoo@gmail.com>
2025-03-14 14:37:24 +0530

    commit

---
+++ h.py
@@ -0,0 +1,11 @@
+from os import *
+
+# getcwd()
+print("---")
+print(getcwd())
+chdir("JavaByKunal")
+print(getcwd())
+print("---")
+# arr = listdir()
+# for i in arr: print(i)
+# print(listvolumes())
